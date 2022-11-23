717f66f65795c368eabe22be02f1ec981547e365
ctysingh <chaitanyasingh@gmail.com>
2022-11-23 11:08:46 -0500

    updated method with optional

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,10 @@ public class Main {
         System.out.println( readDuration(props, "b"));
         System.out.println( readDuration(props, "c"));
 
+        System.out.println(readDurationUsingOptional(props, "a"));
+        System.out.println(readDurationUsingOptional(props, "b"));
+        System.out.println(readDurationUsingOptional(props, "c"));
+
         String [] words = new String[5];
         for ( String word: words ) {
            // System.out.println(word);
@@ -43,17 +47,20 @@ public class Main {
     }
 
     private static int readDurationUsingOptional(Properties props, String name) {
-        String val = props.getProperty(name);
-        if(val != null) {
-            try {
-                int i = Integer.parseInt(val);
-                if(i>0) {
-                    return  i;
-                }
-            } catch (NumberFormatException ex){
+       return Optional.ofNullable(props.getProperty(name))
+               .flatMap(OptionalUtility::stringToInt)
+               .filter(i -> i > 0)
+               .orElse(0);
+    }
 
+    private static class OptionalUtility {
+        public static  Optional<Integer> stringToInt(String s) {
+            try {
+                return Optional.of(Integer.parseInt(s));
+            }catch (NumberFormatException ex) {
+                return  Optional.empty();
             }
+
         }
-        return 0;
     }
 }
