70375adbe71c15b675dfeeb01c3c35d809966671
ctysingh <chaitanyasingh@gmail.com>
2022-11-23 09:12:17 -0500

    add method that check null

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.util.Optional;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Properties props = new Properties();
+        props.setProperty("a", "5");
+        props.setProperty("b", "true");
+        props.setProperty("c", "-3");
+
+
+        System.out.println( readDuration(props, "a"));
+        System.out.println( readDuration(props, "b"));
+        System.out.println( readDuration(props, "c"));
 
         String [] words = new String[5];
         for ( String word: words ) {
@@ -16,5 +25,35 @@ public class Main {
             }
 
         }
+    }
+
+    private static int readDuration(Properties props, String name) {
+        String val = props.getProperty(name);
+        if(val != null) {
+            try {
+                int i = Integer.parseInt(val);
+                if(i>0) {
+                    return  i;
+                }
+            } catch (NumberFormatException ex){
+
+            }
+        }
+        return 0;
+    }
+
+    private static int readDurationUsingOptional(Properties props, String name) {
+        String val = props.getProperty(name);
+        if(val != null) {
+            try {
+                int i = Integer.parseInt(val);
+                if(i>0) {
+                    return  i;
+                }
+            } catch (NumberFormatException ex){
+
+            }
+        }
+        return 0;
     }
 }
